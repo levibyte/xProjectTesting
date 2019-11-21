8ad24b75ae899599ab4222d49a9b9c2c8ce3b220
levon.sargsyan <levon.sargsyan@qt.io>
2019-11-21 21:05:50 +0100

    Update test-runner

--- scripts/bin/runTests.sh
+++ scripts/bin/runTests.sh
@@ -5,26 +5,33 @@ if [ "$runname" == "" ]; then
 	runname="LOCAL_RUN"
 fi
 
-suiteid=$SUITEID
+suiteid=$2
 if [ "$suiteid" == "" ]; then
 	suiteidstr=""
 else
-	suiteidstr="--tr-testrun--suite_id=$suiteid"
+	suiteidstr="--tr-testrun-suite_id=$suiteid"
 fi
 
-suite=$SUITE
-if [ "$suite" == "" ]; then
-	suitestr="tests/$suite/"
+suite=$4
+if [ "$suite" != "" ]; then
+	suitestr="tests/$suite/*"
 else
 	suitestr="tests/"
 fi
 
 
-if [ "$TRUSR" == "" ] || [ "$TRPWD" == "" ]; then
+if [ "$TRUSR" = "" ] || [ "$TRPWD" == "" ]; then
 	echo "Error: set username password first"
 	exit 1
+else
+	if [ "$TEAMCITY" == "" ]; then
+		cp etc/testrail.cfg testrail.cfg.override 
+		sed -i "s/!TC_TESTRAIL_USR!/$TRUSR/" testrail.cfg.override 
+		sed -i "s/!TC_TESTRAIL_PWD!/$TRPWD/" testrail.cfg.override 
+		sed -i "s/!TC_TESTRAIL_SID!/$suiteid/" testrail.cfg.override 
+	fi
 fi
 
-echo "Inovcation: pytest --cov=xProject --testdox --testrail $suiteidstr --tr-email=$TRUSR --tr-password=$TRPWD --tr-testrun-name="$runname" --tr-config=etc/testrail.cfg $suitestr "
-pytest --cov=xProject --testdox --testrail $suiteidstr --tr-email=$TRUSR --tr-password=$TRPWD --tr-testrun-name="$runname" --tr-config=etc/testrail.cfg $suitestr 
+echo "Inovcation: pytest --cov=xProject --testdox --testrail $suiteidstr --tr-testrun-name="$runname" --tr-config=testrail.cfg.override $suitestr "
+pytest --cov=xProject --testdox --testrail --tr-testrun-name="$runname" --tr-config=etc/testrail.cfg $suitestr 
 coverage html
