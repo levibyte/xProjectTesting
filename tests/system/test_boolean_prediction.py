8e33d1ad80e6a6f4b0a0bdf54ba4c5fbc89b932b
levon.sargsyan <levon.sargsyan@qt.io>
2019-11-22 03:55:22 +0100

    Add more comments

--- tests/system/test_boolean_prediction.py
+++ tests/system/test_boolean_prediction.py
@@ -12,15 +12,19 @@ from xProject import NeuronLayer
 @testrail('C52')
 def test_ai_can_predict_the_or_operation():
 	"""
-	System/behavior testing of NeuralNetwork predicting
+	System/behavior test of NeuralNetwork trying to predict
 	the behavior of two bit OR operation. 
 
-	Inputs:
+	Args:
+		- Hidden layers : 5
+		- Training iteration: 1000
+
+	TrainSets:
 		- [0, 0] -> 0
 		- [0, 1] -> 1
 		- [1, 0] -> 1
 
-	Epected:
+	Expectation:
 		- [1, 1] => 1
 		
 	"""
@@ -41,6 +45,24 @@ def test_ai_can_predict_the_or_operation():
 
 @testrail('C53')
 def test_ai_can_predict_the_xor_operation():
+	"""
+	System/behavior test of NeuralNetwork trying to predict
+	the behavior of two bit XOR operation. 
+
+	Args:
+		- Hidden layers : 3
+		- Training iteration: 10000
+		- Training strategy: random value from train set
+
+	TrainSets:
+		- [0, 0] -> 0
+		- [0, 1] -> 1
+		- [1, 0] -> 1
+
+	Expectation:
+		- [1, 1] => 1
+		
+	"""
 	training_sets = [
 		[[0, 0], [0]],
 		[[0, 1], [1]],
@@ -57,6 +79,24 @@ def test_ai_can_predict_the_xor_operation():
 
 @testrail('C54')
 def test_ai_can_predict_the_and_operation():
+	"""
+	System/behavior test of NeuralNetwork trying to predict
+	the behavior of two bit AND operation. 
+
+	Args:
+		- Hidden layers : 3
+		- Training iteration: 10000
+		- Training strategy: random value from train set
+
+	TrainSets:
+		- [0, 0] -> 0
+		- [0, 1] -> 1
+		- [1, 0] -> 1
+
+	Expectation:
+		- [1, 1] => 0
+		
+	"""
 	training_sets = [
 		[[0, 0], [0]],
 		[[1, 0], [0]],
@@ -73,6 +113,10 @@ def test_ai_can_predict_the_and_operation():
 
 @testrail('C56')
 def test_ai_can_predict_the_or_operation_on_3_inputs():
+	"""
+	System/behavior test of NeuralNetwork trying to predict
+	the behavior of 3 bit OR operation. 
+	"""
 	training_sets = [
 		[[0, 0, 0], [0]],
 		[[0, 0, 1], [1]],
@@ -93,6 +137,28 @@ def test_ai_can_predict_the_or_operation_on_3_inputs():
 
 @testrail('C57')
 def test_ai_can_predict_the_sum_operation_on_3_bits():
+	"""
+	System/behavior test of NeuralNetwork trying to predict
+	the behavior of 3 bit SUM operation. 
+
+	Args:
+		- Hidden layers : 12
+		- Training iteration: 100000
+		- Training strategy: random value from train set
+
+	TrainSets:
+		- [0, 0, 0] => [0, 0, 1]
+		- [0, 0, 1] => [0, 1, 0]
+		- [0, 1, 1] => [1, 0, 0]
+		- [1, 0, 0] => [1, 0, 1]
+		- [1, 0, 1] => [1, 1, 0]
+		- [1, 1, 0] => [1, 1, 1]
+		- [1, 1, 1] => [0, 0, 0]
+
+	Expectation:
+		- [[0, 1, 0], [0, 1, 1]],
+		
+	"""
 	training_sets = [
 		[[0, 0, 0], [0, 0, 1]],
 		[[0, 0, 1], [0, 1, 0]],
@@ -117,7 +183,11 @@ def test_ai_can_predict_the_sum_operation_on_3_bits():
 	#print ("Predication SUM when input is [0,1,0] --> {}".format(nn.feed_forward([0,1,0])))
 
 @testrail('C58')
-def test_ai_can_predict_the_or_operation_on_3bits():
+def test_ai_can_predict_the_xor_operation_on_3bits():
+	"""
+	System/behavior test of NeuralNetwork trying to predict
+	the behavior of 3 bit OR operation. 
+	"""
 	training_sets = [
 		[[0, 0], [0]],
 		[[0, 1], [1]],
--- tests/unit/neuron_test.py
+++ tests/unit/neuron_test.py
@@ -10,33 +10,39 @@ from xProject import Neuron
 def test_construction_of_dummy_neuron():
 	"""
 	Tests construction of Neuron Object
+	checks that bias and weights have expected values
 
 	Args:
 		- None
 
-	Return:
-		- None
+	Expected:
+		- Weights should be uninitilaizd
+		- Bias should be unchanged
 
 	"""
 	neuron = Neuron.Neuron(0.1)
+	assert neuron.bias == 0.1
+	assert neuron.weights == []
+	
+	
 
 @testrail('C62')
 @mock.patch('xProject.Neuron.Neuron.squash')
 def test_calculating_the_output(mock_squash):
 	"""
-	Tests behavior of ouput value calculation
-	Given input i1, i2, i3 , weights w1, w2, w2 and bias "B" 
-	the output should be  total += .inputs[i] * .weights[i]
-
+	Tests the behavior of ouput value calculation
+	Given input i, the return value o should be calculated as follows
+	o = squash(i)
+	
 	Mocks:
-		- Neuron.Squash()
+		- Neuron.Squash() => always returns 1
 
 	Inputs:
-		- None
+		- [ 1, 1, 5 ]
 
 	Epected:
-		- calculate_output to be 1
-		- sqush member to be called
+		- Neuron.calculate_output() returns 1 
+		- Neuron.squash() should be called
 		
 	"""
 	mock_squash.return_value = 1
@@ -46,25 +52,56 @@ def test_calculating_the_output(mock_squash):
 	assert neuron.calculate_output(inputs) == 1 
 	mock_squash.assert_called()
 
-@testrail('C63')
-def test_consistency_of_squash_function():
+@testrail('C72')
+def test_calculating_total_net_input():
 	"""
-	Tests behavior of squash function
+	Tests behavior of Neuron.calculate_total_net_input() function
 	Given input i1, i2, i3 , weights w1, w2, w2 and bias "B" 
 	the output should be  total += .inputs[i] * .weights[i]
 
 	Inputs:
-		- total_net_input
+		- neuron.weights = [0.2, 0.4, 0.1]
+		- neuron.inputs = [ 1, 1, 5 ]
 
 	Epected:
-		- squash return value to be 1 / (1 + math.exp(-total_net_input))
+		- the output value should be beetween 1.1><1.3
+		
+	"""
+	neuron = Neuron.Neuron(0.1)
+	neuron.weights = [0.2, 0.4, 0.1]
+	neuron.inputs = [ 1, 1, 5 ]
+	assert neuron.calculate_total_net_input() > 1.1
+	assert neuron.calculate_total_net_input() < 1.3
+	
 
+@testrail('C63')
+@pytest.mark.parametrize("i,exp_range_begin,exp_range_end",[
+    (0.9,0.6,0.8),
+    (1.9,0.2,0.9),
+    (-0.9,0.1,0.8)],
+)
+def test_consistency_of_squash_function(i,exp_range_begin,exp_range_end):
+	"""
+	Tests behavior of Neuron.squash() function
+	the expected return value of function should be according following formula
+	res = 1 / (1 + e^(-i))
+
+	Inputs:
+		- case1 : 0.9
+		- case2 : -0.9
+		- case3 : 1.9
+
+	Epected:
+		- case1 : a < return value < b
+		- case2 : a < return value < b
+		- case3 : a < return value < b
+		
 	"""
 
-	input_val = 0.9
+	input_val = i
 	neuron = Neuron.Neuron(0.1)
-	assert neuron.squash(input_val) < 1
-	assert neuron.squash(input_val) > 0
+	assert neuron.squash(input_val) < exp_range_end
+	assert neuron.squash(input_val) > exp_range_begin
 
 @testrail('C64')
 @pytest.mark.parametrize("o1,o2,expected",[
